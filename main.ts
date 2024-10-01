2809b033efcbce8edc467980370f6076d5f18254
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-01 11:24:39 -0400

    aMade the clicker.

--- main.ts
+++ main.ts
@@ -2,7 +2,21 @@
  *
  * Created by: Emre Guzel
  * Created on: Oct 1 2024
- * This program ...
+ * This program is cocike cilcker 
 */
 
 //basic.showString('Hello, World!')
+basic.clearScreen()
+let cocikeNumber=+1
+let cocikeRest=0
+basic.showIcon(IconNames.Happy)
+input.onButtonPressed(Button.B,function(){
+    cocikeRest=0
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.A,function(){
+ cocikeNumber += 1
+ basic.showNumber(1)
+})
+
+
