fcfbf74c1248c1ad1542b1ae18595e76b838866d
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-01 23:03:35 +0000

    made the cookie counter

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,19 +1,18 @@
 """
 Created by: Emre Guzel 
 Created on: Oct 1 2024
-This module is a Micro:bit cookie clicker 
+This module is a Micro:bit cookie counter 
 """
-
 from microbit import *
-
 display.clear()
-
 display.show(Image.HAPPY)
-#Setting the button B#
+cookie_count=0
+
 while True:
-    if button_b.get_presses():
-        cookie_rest=0
-        display.scroll(cookie_rest + )
-        if button_a.get_presses():
-            cookie_number = + 1
-            display.scroll(cookie_number -1)
+    if button_b.is_pressed():
+        cookie_count=0
+        display.scroll(cookie_count)
+
+    if button_a.is_pressed():
+        cookie_count += 1
+        display.scroll(cookie_count)
