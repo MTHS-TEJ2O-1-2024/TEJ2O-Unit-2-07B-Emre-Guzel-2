0c83d6bd827d8ca6e7f8be81f2c79451daf3dadc
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-01 17:30:28 +0000

    strated to maing the cookie clicker

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,19 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Emre Guzel 
+Created on: Oct 1 2024
+This module is a Micro:bit cookie clicker 
 """
 
 from microbit import *
 
+display.clear()
 
-display.scroll("Hello, World!")
+display.show(Image.HAPPY)
+#Setting the button B#
+while True:
+    if button_b.get_presses():
+        cookie_rest=0
+        display.scroll(cookie_rest + )
+        if button_a.get_presses():
+            cookie_number = + 1
+            display.scroll(cookie_number -1)
